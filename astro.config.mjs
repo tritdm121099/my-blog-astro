cc94720387f919d388f37ac0892b67157d057f29
Tri <ninqwe123@gmail.com>
2023-10-08 22:54:22 +0700

    [feat] completed learn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), preact()]
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,11 @@
-import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
-  const posts = await getCollection("blog");
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
     site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/blog/${post.slug}/`,
-    })),
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
   });
 }

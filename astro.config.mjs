2a083edec880d1e0c672bfbd09ca10d9f982ee82
Tri <ninqwe123@gmail.com>
2023-10-22 23:05:41 +0700

    [feat] setup base url blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  site: 'https://tdmtri99.github.io',
+  base: '/my-blog-astro',
+  integrations: [mdx(), sitemap(), tailwind(),],
 });

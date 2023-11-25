6e6d629eccc0b71861afa6de2144c04500a20808
Tri <ninqwe123@gmail.com>
2023-11-25 13:44:00 +0700

    [ci] update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import astroExpressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tdmtri99.github.io",
-  base: "",
-  integrations: [astroExpressiveCode(), mdx(), sitemap(), tailwind()],
+  base: "my-blog-astro",
+  integrations: [astroExpressiveCode(), mdx(), tailwind()],
 });

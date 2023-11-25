4a33b861ac5a752a77f8efce453e242bdf4a7fdc
Tri <ninqwe123@gmail.com>
2023-11-25 14:39:47 +0700

    [fix] correct href path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import astroExpressiveCode from "astro-expressive-code";
+import { BASE } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tdmtri99.github.io",
-  base: "/my-blog-astro",
+  base: BASE,
   integrations: [astroExpressiveCode(), mdx(), tailwind()],
 });

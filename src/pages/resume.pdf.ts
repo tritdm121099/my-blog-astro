3dcc64ab2452807e0b012a03add7bbabee2e7126
Tri <tritruong121099@gmail.com>
2024-07-03 22:37:43 +0700

    [feat] setup pdf font

--- src/pages/resume.pdf.ts
+++ src/pages/resume.pdf.ts
@@ -1,9 +1,19 @@
 import { jsPDF } from "jspdf";
+import "@assets/fonts/JetBrainsMono-Regular-normal";
+import { BASE } from "../consts";
 
 export async function GET({  }) {
 	const doc = new jsPDF();
 	doc.text("Hello world!", 10, 10);
 	const buffer = doc.output('arraybuffer')
+export async function GET({}) {
+  const doc = new jsPDF();
+  doc.setFont("JetBrainsMono-Regular");
 
 	return new Response(buffer);
-  }
+  }  const buffer = doc.output("arraybuffer");
+  doc.text("Tri Truong Dinh Minh", 10, 10);
+  const buffer = doc.output("arraybuffer");
+
+  return new Response(buffer);
+}
